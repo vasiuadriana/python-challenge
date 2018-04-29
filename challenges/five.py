4272b1b42005b7cc193ea78ea763e093ecbe3e38
Adriana Vasiu <vasiu.adriana@gmail.com>
2018-04-29 09:00:13 +0100

    Solve challenge five

--- challenges/five.py
+++ challenges/five.py
@@ -1,1 +1,16 @@
 # http://www.pythonchallenge.com/pc/def/peak.html
+import urllib.request
+import pickle
+
+
+def solve_challenge():
+    url = 'http://www.pythonchallenge.com/pc/def/banner.p'
+    with urllib.request.urlopen(url) as f:
+        pickled_obj = pickle.load(f)
+        for el in pickled_obj:
+            output = ''.join([e[0] * e[1] for e in el])
+            print(output)
+
+
+if __name__ == '__main__':
+    solve_challenge()
