bb77c32d7f3b8ea69987c60b89e7077aef3e21ab
Adriana Vasiu <vasiu.adriana@gmail.com>
2018-05-05 10:04:50 +0100

    Solve challenge 8

--- challenges/challenge_8.py
+++ challenges/challenge_8.py
@@ -1,1 +1,16 @@
 # http://www.pythonchallenge.com/pc/def/integrity.html
+import bz2
+
+
+def solve_challenge():
+    data = {
+        'un': b'BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084',
+        'pw': b'BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08'
+    }
+
+    print('Username is %s ' % bz2.decompress(data['un']))
+    print('Username is %s ' % bz2.decompress(data['pw']))
+
+
+if __name__ == '__main__':
+    solve_challenge()
