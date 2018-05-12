db3fecd4a95dd3a9b48f3c1084eb21c654bb7dd4
Adriana Vasiu <vasiu.adriana@gmail.com>
2018-05-12 19:35:41 +0100

    Solve challenge 11

--- challenges/challenge_11.py
+++ challenges/challenge_11.py
@@ -1,1 +1,24 @@
 # http://www.pythonchallenge.com/pc/return/5808.html
+from PIL import Image
+
+
+def solve_challenge():
+    image = Image.open('data/cave.jpg')
+    (w, h) = image.size
+
+    even = Image.new('RGB', (w // 2, h // 2))
+    odd = Image.new('RGB', (w // 2, h // 2))
+
+    for i in range(w):
+        for j in range(h):
+            p = image.getpixel((i, j))
+            if (i + j) % 2 == 1:
+                odd.putpixel((i // 2, j // 2), p)
+            else:
+                even.putpixel((i // 2, j // 2), p)
+    even.save('data/even.jpg')
+    odd.save('data/odd.jpg')
+
+
+if __name__ == '__main__':
+    solve_challenge()
